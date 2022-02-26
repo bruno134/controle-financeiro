74b015bca9b1702fbfbb3ffe9c36e2229f07197a
bruno134 <brunodn@gmail.com>
2022-02-26 10:53:43 -0300

    Ajuste profile nas configurações

--- src/main/java/br/com/mcf/controlefinanceiro/config/PeriodoMesConfig.java
+++ src/main/java/br/com/mcf/controlefinanceiro/config/PeriodoMesConfig.java
@@ -1,6 +1,5 @@
 package br.com.mcf.controlefinanceiro.config;
 
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
