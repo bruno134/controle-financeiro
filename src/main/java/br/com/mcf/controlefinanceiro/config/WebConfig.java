67d6297bfcc3149bb0afa173d39add3834d90d22
bruno134 <brunodn@gmail.com>
2023-05-07 19:06:08 -0300

    unused import removed

--- src/main/java/br/com/mcf/controlefinanceiro/config/WebConfig.java
+++ src/main/java/br/com/mcf/controlefinanceiro/config/WebConfig.java
@@ -1,7 +1,6 @@
 package br.com.mcf.controlefinanceiro.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
